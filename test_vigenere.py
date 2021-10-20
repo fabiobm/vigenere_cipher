4450b119375e7c1d1aff11017a19f1029210b1c2
Fabio Brzostek Muller <fabiobrzm@gmail.com>
2021-10-20 19:48:56 -0300

    Add formatting

--- test_vigenere.py
+++ test_vigenere.py
@@ -2,110 +2,135 @@ from string import digits
 from vigenere import Vigenere
 from unittest import TestCase
 
+
 class TestVigenere(TestCase):
     def test_ascii_alphabet(self):
         vigenere = Vigenere()
 
-        self.assertEqual(vigenere.encrypt('banana', 'a'), 'banana')
-        self.assertEqual(vigenere.encrypt('banana', 'b'), 'cbobob')
-        self.assertEqual(vigenere.encrypt('banana', 'aba'), 'bbnaoa')
-        self.assertEqual(vigenere.encrypt('banana', 'lemon'), 'mezoal')
-        self.assertEqual(vigenere.encrypt('banana', 'banana'), 'caaaaa')
+        self.assertEqual(vigenere.encrypt("banana", "a"), "banana")
+        self.assertEqual(vigenere.encrypt("banana", "b"), "cbobob")
+        self.assertEqual(vigenere.encrypt("banana", "aba"), "bbnaoa")
+        self.assertEqual(vigenere.encrypt("banana", "lemon"), "mezoal")
+        self.assertEqual(vigenere.encrypt("banana", "banana"), "caaaaa")
 
-        self.assertEqual(vigenere.decrypt('banana', 'a'), 'banana')
-        self.assertEqual(vigenere.decrypt('cbobob', 'b'), 'banana')
-        self.assertEqual(vigenere.decrypt('bbnaoa', 'aba'), 'banana')
-        self.assertEqual(vigenere.decrypt('mezoal', 'lemon'), 'banana')
-        self.assertEqual(vigenere.decrypt('caaaaa', 'banana'), 'banana')
+        self.assertEqual(vigenere.decrypt("banana", "a"), "banana")
+        self.assertEqual(vigenere.decrypt("cbobob", "b"), "banana")
+        self.assertEqual(vigenere.decrypt("bbnaoa", "aba"), "banana")
+        self.assertEqual(vigenere.decrypt("mezoal", "lemon"), "banana")
+        self.assertEqual(vigenere.decrypt("caaaaa", "banana"), "banana")
 
     def test_digits(self):
         vigenere = Vigenere(list(digits))
 
-        self.assertEqual(vigenere.encrypt('1234', '0'), '1234')
-        self.assertEqual(vigenere.encrypt('1234', '420'), '5438')
+        self.assertEqual(vigenere.encrypt("1234", "0"), "1234")
+        self.assertEqual(vigenere.encrypt("1234", "420"), "5438")
 
-        self.assertEqual(vigenere.decrypt('1234', '0'), '1234')
-        self.assertEqual(vigenere.decrypt('5438', '420'), '1234')
+        self.assertEqual(vigenere.decrypt("1234", "0"), "1234")
+        self.assertEqual(vigenere.decrypt("5438", "420"), "1234")
 
     def test_mixed_case(self):
-        vigenere = Vigenere(['a', 'b', 'c', 'd', 'A', 'B', 'C', 'D'])
+        vigenere = Vigenere(["a", "b", "c", "d", "A", "B", "C", "D"])
 
-        self.assertEqual(vigenere.encrypt('aAbB', 'a'), 'aAbB')
-        self.assertEqual(vigenere.encrypt('aAbB', 'cDaB'), 'cdbc')
+        self.assertEqual(vigenere.encrypt("aAbB", "a"), "aAbB")
+        self.assertEqual(vigenere.encrypt("aAbB", "cDaB"), "cdbc")
 
-        self.assertEqual(vigenere.decrypt('aAbB', 'a'), 'aAbB')
-        self.assertEqual(vigenere.decrypt('cdbc', 'cDaB'), 'aAbB')
+        self.assertEqual(vigenere.decrypt("aAbB", "a"), "aAbB")
+        self.assertEqual(vigenere.decrypt("cdbc", "cDaB"), "aAbB")
 
     def test_unicode(self):
-        vigenere = Vigenere([
-            'á', 'à', 'â', 'ã', 'ä', 'é', 'è', 'ê', 'ë', 'í',
-            'ì', 'ï', 'ó', 'ò', 'õ', 'ö', 'ú', 'ù', 'ü'
-        ])
+        vigenere = Vigenere(
+            [
+                "á",
+                "à",
+                "â",
+                "ã",
+                "ä",
+                "é",
+                "è",
+                "ê",
+                "ë",
+                "í",
+                "ì",
+                "ï",
+                "ó",
+                "ò",
+                "õ",
+                "ö",
+                "ú",
+                "ù",
+                "ü",
+            ]
+        )
 
-        self.assertEqual(vigenere.encrypt('áéíóú', 'á'), 'áéíóú')
-        self.assertEqual(vigenere.encrypt('áéíóú', 'äïü'), 'äúëúë')
+        self.assertEqual(vigenere.encrypt("áéíóú", "á"), "áéíóú")
+        self.assertEqual(vigenere.encrypt("áéíóú", "äïü"), "äúëúë")
 
-        self.assertEqual(vigenere.decrypt('áéíóú', 'á'), 'áéíóú')
-        self.assertEqual(vigenere.decrypt('äúëúë', 'äïü'), 'áéíóú')
+        self.assertEqual(vigenere.decrypt("áéíóú", "á"), "áéíóú")
+        self.assertEqual(vigenere.decrypt("äúëúë", "äïü"), "áéíóú")
 
     def test_lists(self):
-        vigenere = Vigenere(['bar', 'foo', 'hello', 'man', 'woman', 'world'])
+        vigenere = Vigenere(["bar", "foo", "hello", "man", "woman", "world"])
 
         self.assertEqual(
-            vigenere.encrypt(['hello', 'world'], ['bar']),
-            ['hello', 'world']
+            vigenere.encrypt(["hello", "world"], ["bar"]), ["hello", "world"]
         )
         self.assertEqual(
-            vigenere.encrypt(['hello', 'world'], ['foo', 'bar']),
-            ['man', 'world']
+            vigenere.encrypt(["hello", "world"], ["foo", "bar"]), ["man", "world"]
         )
 
         self.assertEqual(
-            vigenere.decrypt(['hello', 'world'], ['bar']),
-            ['hello', 'world']
+            vigenere.decrypt(["hello", "world"], ["bar"]), ["hello", "world"]
         )
         self.assertEqual(
-            vigenere.decrypt(['man', 'world'], ['foo', 'bar']),
-            ['hello', 'world']
+            vigenere.decrypt(["man", "world"], ["foo", "bar"]), ["hello", "world"]
         )
 
     def test_invalid_alphabet(self):
         with self.assertRaises(ValueError) as e:
-            Vigenere('abcd')
-            self.assertEqual(str(e.exception), 'Alphabet must be a list of characters or strings')
+            Vigenere("abcd")
+            self.assertEqual(
+                str(e.exception), "Alphabet must be a list of characters or strings"
+            )
 
         with self.assertRaises(ValueError) as e:
-            Vigenere('abcda')
-            self.assertEqual(str(e.exception), 'Alphabet must not have duplicated symbols')
+            Vigenere("abcda")
+            self.assertEqual(
+                str(e.exception), "Alphabet must not have duplicated symbols"
+            )
 
     def test_invalid_text(self):
         with self.assertRaises(ValueError) as e:
             vigenere = Vigenere()
-            vigenere.encrypt(123, 'aaa')
-            self.assertEqual(str(e.exception), 'Text must be a string or a list of strings')
+            vigenere.encrypt(123, "aaa")
+            self.assertEqual(
+                str(e.exception), "Text must be a string or a list of strings"
+            )
 
     def test_invalid_key(self):
         vigenere = Vigenere()
 
         with self.assertRaises(ValueError) as e:
-            vigenere.encrypt('', '')
-            self.assertEqual(str(e.exception), 'Key must not be empty')
+            vigenere.encrypt("", "")
+            self.assertEqual(str(e.exception), "Key must not be empty")
 
         with self.assertRaises(ValueError) as e:
-            vigenere.encrypt('', 123)
-            self.assertEqual(str(e.exception), 'Key must be a string or a list of strings')
+            vigenere.encrypt("", 123)
+            self.assertEqual(
+                str(e.exception), "Key must be a string or a list of strings"
+            )
 
     def test_text_not_in_alphabet(self):
         with self.assertRaises(ValueError) as e:
             vigenere = Vigenere()
-            vigenere.encrypt('abcD', 'banana')
+            vigenere.encrypt("abcD", "banana")
 
-            self.assertEqual(str(e.exception), 'Symbols in text must be in the alphabet')
+            self.assertEqual(
+                str(e.exception), "Symbols in text must be in the alphabet"
+            )
 
     def test_key_not_in_alphabet(self):
         with self.assertRaises(ValueError) as e:
             vigenere = Vigenere()
-            vigenere.decrypt('abcdefgh', 'bananA')
+            vigenere.decrypt("abcdefgh", "bananA")
 
-            self.assertEqual(str(e.exception), 'Symbols in key must be in the alphabet')
-
+            self.assertEqual(str(e.exception), "Symbols in key must be in the alphabet")
--- vigenere.py
+++ vigenere.py
@@ -1,43 +1,44 @@
 from string import ascii_lowercase
 
+
 class Vigenere:
-    '''
+    """
     Encrypts and decrypts text using the [Vigenère cypher](https://en.wikipedia.org/wiki/Vigenère_cipher).
 
     The alphabet is by default the lowercase Latin alphabet, but can be any
     list of (unique) characters or strings.
-    '''
+    """
 
     def __init__(self, alphabet=list(ascii_lowercase)):
         if not isinstance(alphabet, list):
-            raise ValueError('Alphabet must be a list of characters or strings')
+            raise ValueError("Alphabet must be a list of characters or strings")
         if len(set(alphabet)) != len(alphabet):
-            raise ValueError('Alphabet must not have duplicated symbols')
+            raise ValueError("Alphabet must not have duplicated symbols")
 
         self.alphabet = alphabet
         self.words = any(len(symbol) > 1 for symbol in alphabet)
 
     def _transform(self, text, key, decrypting=False):
-        if key == '' or key == []:
-            raise ValueError('Key must not be empty')
+        if key == "" or key == []:
+            raise ValueError("Key must not be empty")
 
         if not isinstance(key, str) and not isinstance(key, list):
-            raise ValueError('Key must be a string or a list of strings')
+            raise ValueError("Key must be a string or a list of strings")
 
         if not isinstance(text, str) and not isinstance(text, list):
-            raise ValueError('Text must be a string or a list of strings')
+            raise ValueError("Text must be a string or a list of strings")
 
         transformed = []
         for i, symbol in enumerate(text):
             try:
                 symbol_idx = self.alphabet.index(symbol)
             except ValueError:
-                raise ValueError('Symbols in text must be in the alphabet')
+                raise ValueError("Symbols in text must be in the alphabet")
 
             try:
                 key_symbol_idx = self.alphabet.index(key[i % len(key)])
             except ValueError:
-                raise ValueError('Symbols in key must be in the alphabet')
+                raise ValueError("Symbols in key must be in the alphabet")
 
             idx = (
                 symbol_idx + (key_symbol_idx if not decrypting else -key_symbol_idx)
@@ -45,7 +46,7 @@ class Vigenere:
 
             transformed.append(self.alphabet[idx])
 
-        return transformed if self.words else ''.join(transformed)
+        return transformed if self.words else "".join(transformed)
 
     def encrypt(self, text, key):
         """
